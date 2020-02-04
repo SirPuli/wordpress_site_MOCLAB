bd5d2f60ee50f9ad44b01c442241d4221ec1ba73
Verega <60603126+Verega@users.noreply.github.com>
2020-02-04 12:35:46 +0100

    404 Page

---
+++ views/404.php
@@ -0,0 +1,12 @@
+<div style="text-align: center;">	
+
+	<p>
+		<img src="/wordpress/wp-content/uploads/2020/02/mistake-1194670_640.png">
+	</p>
+
+	<p>
+		<img src="/wordpress/wp-content/uploads/2020/02/404error.png">
+	</p>
+	
+
+</div>
