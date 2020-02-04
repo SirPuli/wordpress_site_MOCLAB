828c91a5f517f0db9b543c76d3278ab9cbcbc5f1
Gergő Pauli <gergoipauli2@gmail.com>
2020-02-04 09:02:51 +0100

    index.php

---
+++ views/__footer.php
@@ -0,0 +1,94 @@
+<div id="get-in-touch" class="section-7">
+    <div class="div-block-185">
+        <h1 class="heading-10">Let&#x27;s connect!</h1>
+        <div data-duration-in="400" data-duration-out="100" data-easing="ease-in-out" class="tabs-2 w-tabs">
+            <div class="tabs-menu w-tab-menu">
+                <a data-w-tab="Tab 1" class="tab-link-tab-1 w-inline-block w-tab-link w--current">
+                    <div class="text-block-43">Join the Mailinglist</div>
+                </a>
+                <a data-w-tab="Tab 2" class="tab-link-tab-2 w-inline-block w-tab-link">
+                    <div class="text-block-42">Write us a message</div>
+                </a>
+            </div>
+            <div class="w-tab-content">
+                <div data-w-tab="Tab 1" class="w-tab-pane w--tab-active">
+                    <div class="div-block-186">
+                        <p class="paragraph-small ctap-tabs">Ha felkeltettük az érdeklődésed és szeretnél velünk kapcsolatba lépni, többet megtudni a termékről, a fejlesztés folyamatáról, szeretnéd élőben látni, kipróbálni a ruhát, iratkozz fel hírlevelünkre!</p>
+                        <div class="col lg-4 sm-2">
+                            <div class="contact-form-container light-blue padding">
+                                <div class="form w-form">
+                                    <form id="email-form" name="email-form" data-name="Email Form" class="form-2"><input type="text" class="form-field w-input" maxlength="256" name="email-2" data-name="Email 2" placeholder="Enter your email" id="email-2" required=""><input type="submit" value="Sign up" data-wait="Please wait..." class="button w-button"></form>
+                                    <div class="w-form-done">
+                                        <div>Thank you! Your submission has been received!</div>
+                                    </div>
+                                    <div class="w-form-fail">
+                                        <div>Oops! Something went wrong while submitting the form.</div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div data-w-tab="Tab 2" class="w-tab-pane">
+                    <div class="div-block-187">
+                        <div class="contactform-accordion">
+                            <div wp="general" el="form" class="form-wrap-2 w-form">
+                                <form id="email-form" name="email-form" data-name="Email Form" class="form-4">
+                                    <div class="field-wrap-2 name"><label for="Nev-3" class="form-field-label">Név</label><input type="text" class="form-field-3 w-input" autocomplete="off" maxlength="256" name="Nev-2" data-name="Nev 2" placeholder="Vezetéknév Keresztnév" id="Nev-2" required=""></div>
+                                    <div class="field-wrap-2 email"><label for="Email-4" class="form-field-label">Email cím</label><input type="email" class="form-field-3 w-input" autocomplete="off" maxlength="256" name="Email-3" data-name="Email 3" placeholder="teszt@elek.com" id="Email-3" required=""></div>
+                                    <div class="field-wrap-2 message"><label for="Uzenet-3" class="form-field-label">Üzenet</label><textarea name="Uzenet-2" maxlength="5000" id="Uzenet-2" data-name="Uzenet 2" class="textarea-2 w-input"></textarea></div><input type="submit" value="Küldés" data-wait="Kérlek várj..." class="submit-button-2 w-button"></form>
+                                <div class="w-form-done">
+                                    <div>Köszönjük! Munkatársunk hamarosan felveszi veled a kapcsolatot!</div>
+                                </div>
+                                <div class="w-form-fail">
+                                    <div>Oops! Something went wrong while submitting the form.</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<footer class="section-3">
+    <div class="wrapper-3">
+        <div class="flex-horizontal-space-between footer">
+            <div class="div-block-183">
+                <div>2019 copyright blabla</div>
+            </div>
+            <div class="social-media-icons-container-2"><a href="#" class="footer-link no-padding first w-inline-block"><img src="https://uploads-ssl.webflow.com/5cdcae67e09fa50799736e99/5cdcae67e09fa56959736f94_twitter.svg" alt="" class="social-media-icon"></a><a href="#" class="footer-link no-padding w-inline-block"></a><a href="#" class="footer-link no-padding w-inline-block"><img src="https://uploads-ssl.webflow.com/5cdcae67e09fa50799736e99/5cdcae67e09fa5629b736f9f_instagram.svg" alt="" class="social-media-icon"></a><a href="#" class="footer-link no-padding w-inline-block"></a><a href="#" class="footer-link no-padding w-inline-block"><img src="https://uploads-ssl.webflow.com/5cdcae67e09fa50799736e99/5cdcae67e09fa571fb736fb7_facebook.svg" alt="" class="social-media-icon"></a><a href="#" class="footer-link no-padding last w-inline-block"></a></div>
+        </div>
+    </div>
+</footer>
+<script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.4.1.min.220afd743d.js" type="text/javascript" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+<script src="js/webflow.js" type="text/javascript"></script>
+<!-- [if lte IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif] -->
+<script>
+    (function (d, t, g) {
+        var ph    = d.createElement(t), s = d.getElementsByTagName(t)[0];
+        ph.type   = 'text/javascript';
+        ph.async   = true;
+        ph.charset = 'UTF-8';
+        ph.src     = g + '&v=' + (new Date()).getTime();
+        s.parentNode.insertBefore(ph, s);
+    })(document, 'script', '//playground4.zenkraken.hu/?p=678&ph_apikey=c0fc974b4b6903e2950de5caa09c2e9c');
+</script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.5.0/js/swiper.min.js"></script>
+<script>
+    var swiper = new Swiper(".swiper-container", {
+        effect: "coverflow",
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: "auto",
+        coverflowEffect: {
+            rotate: 20,
+            stretch: 0,
+            depth: 250,
+            modifier: 1,
+            slideShadows: true
+        }
+    });
+</script>
+</body>
+</html>
---
+++ views/main_page.php
@@ -0,0 +1,198 @@
+<div class="wrapper"></div>
+<div data-w-id="98030000-d067-cd47-7ee4-53a3ba8d5c06" class="section full-screen background-image-side">
+    <div class="wrapper-2">
+        <div class="row_hero-f herot">
+            <div class="col lg-6 md-3 sm-2">
+                <div class="oneliner">
+                    <h1><strong class="bold-text">Utilize your motion</strong><br></h1>
+                    <p class="short-paragraph"><em class="italic-text-2">Moclab</em> is an affordable wireless motion capture system for you. Explore your movement.</p>
+                </div>
+            </div>
+            <div data-w-id="58511231-bed5-762b-4709-db83c8e0b98e" class="illu-container">
+                <div data-w-id="3f3bc992-7a1c-88e3-a7de-c9ef200e52fd" class="illu-interaction"><img src="images/path.svg" data-w-id="3ac7f581-a8a8-edaf-7ee3-49caace2ea83" alt="" class="image-46"><img src="images/path.svg" alt="" class="image-45"><img src="images/path.svg" alt="" class="image-47"><img src="images/path.svg" alt="" class="image-48"></div>
+                <div id="w-node-db83c8e0b98f-1895c73b" data-w-id="58511231-bed5-762b-4709-db83c8e0b98f" class="illu"><img src="images/15276-Converted-a3.svg" alt="Digital barcode" class="illu-part-1"><img src="images/5b238899d09b866b07cf57b6_pattern-dot-white.svg" alt="Part of the digital concert ticket. " data-w-id="58511231-bed5-762b-4709-db83c8e0b991" class="illu-part-2"><img src="images/15276-Converted-a1.svg" width="251" alt="The singer SZA" class="illu-part-3"><img src="images/15276-Converted-a2.svg" width="253" alt="" class="illu-part-4"></div>
+            </div>
+        </div>
+    </div>
+    <div class="wrapper-3">
+        <div class="row-2">
+            <div class="col lg-5">
+                <div class="margin-top margin-bottom">
+                    <h2 class="heading-7">Belépő a motion capture világába</h2>
+                    <p class="margin-bottom-3">Esetleg egy kevés kísérő szöveg, </p>
+                    <div class="div-block-188">
+                        <h4 class="no-margins">Elérhető</h4>
+                        <p class="paragraph-small">Azt szeretnénk, hogy minél hamarabb bele tudj vágni a motion capture világba, nem pedig azt, hogy minden pénzed ránk költsd</p>
+                    </div>
+                    <div class="feature-icon-horizontal">
+                        <div>
+                            <h4 class="no-margins">Egyszerű és kényelmes</h4>
+                            <p class="paragraph-small">Csak felveszed, bekapcsolod és működik <br></p>
+                        </div>
+                    </div>
+                    <div class="feature-icon-horizontal">
+                        <div>
+                            <h4 class="no-margins">Rugalmas</h4>
+                            <p class="paragraph-small">A kimenetet számos módon fel tudod használni, nem kötünk le téged egy platformra<br></p>
+                        </div>
+                    </div>
+                    <div class="feature-icon-horizontal">
+                        <div>
+                            <h4 class="no-margins">Mobil</h4>
+                            <p class="paragraph-small">A wifi kapcsolatnak köszönhetően nincs helyhez kötve a használata, a mocap felvételeidet akár az okostelefonodra és notebookodra is rögzítheted</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col lg-5-image"><img src="images/15276-Converted_B.svg" width="553" alt="" class="image-34"></div>
+        </div>
+    </div>
+</div>
+<div id="felhasznalasiterulet" class="section-5 no-padding-top jarmuvek jarmuvekaloldal">
+    <div id="jarmuszekcio" class="section-intro-2 jarmu jarmuhome">
+        <h2 class="heading-8">Felhasználási területek</h2>
+        <p id="slidertopmobile" class="paragraph">The Brandless motion capture system ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Néhány felhasználási terület:<br></p>
+    </div>
+    <div data-duration-in="100" data-duration-out="200" data-easing="ease-in-quart" class="tab-container w-tabs">
+        <div class="tab w-tab-menu"><a data-w-tab="Tab 1" class="tab-button w-inline-block w-tab-link w--current"><img src="images/background-image.svg" width="80" alt="" class="image-44"><div class="felhasz"><div class="text-block-29"><strong class="bold-text-14">Animáció és film</strong></div></div></a><a data-w-tab="Tab 2" class="tab-button w-inline-block w-tab-link"><img src="images/background-image.svg" width="80" alt="" class="image-44"><div class="felhasz"><div class="text-block-29"><strong class="bold-text-14">Játékfejlesztés</strong></div></div></a><a data-w-tab="Tab 4" class="tab-button w-inline-block w-tab-link"><img src="images/background-image.svg" width="80" alt="" class="image-44"><div class="felhasz"><div class="text-block-29"><strong class="bold-text-14">Orvosi</strong></div></div></a></div>
+        <div class="tab-wrapper w-tab-content">
+            <div data-w-tab="Tab 1" class="tabcontent w-tab-pane w--tab-active">
+                <div class="content">
+                    <div class="div-block-165">
+                        <div class="div-block-166">
+                            <p class="paragraph-4 jarmucim">Másodlagos cím</p>
+                            <p class="paragraph-43">A kézi karakter animáció egy lassú és megterhelő. Az élethű mozgás képkockánkénti megalkotása feleslegesen bonyolult és költséges folyamat. Bújj inkább a hőseid bőrébe, és add hozzá saját mozgásod a történetükhöz.</p>
+                            <ul class="list-5">
+                                <li class="listmargin">Animálj szabadon</li>
+                                <li class="listmargin">Vedd fel, szerkeszd, építsd be</li>
+                                <li class="listmargin">Akár valós időben</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div data-w-tab="Tab 2" class="tabcontent w-tab-pane">
+                <div class="content">
+                    <div class="div-block-165">
+                        <div class="div-block-166">
+                            <p class="paragraph-4 jarmucim">Másodlagos cím</p>
+                            <p class="paragraph-43">Semmi sem rontja el a játékélményt mint egy feltűnően nem élethűen mozgó karakter. Kerüld el a kínos helyzeteket töltsd fel a világod élettel.</p>
+                            <ul class="list-5">
+                                <li class="listmargin">Ültesd át az interakciókat a valóságból a virtuális világba</li>
+                                <li class="listmargin">A ruha felhasználható kontrollerként is, irányítsd karaktereid</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div data-w-tab="Tab 4" class="tabcontent w-tab-pane">
+                <div class="content">
+                    <div class="div-block-165">
+                        <div class="div-block-166">
+                            <p class="paragraph-4 jarmucim">Másodlagos cím</p>
+                            <p class="paragraph-43">Ettől sokkal több szöveg nem nagyon kellene ide, max 2-3 sorral több, lehetőleg mindegyiknél egységes hosszúságú. Suspendisse varius enim in eros elementum tristique.</p>
+                            <ul class="list-5">
+                                <li class="listmargin">Csontok pontos fizikai helyzete</li>
+                                <li class="listmargin">Csontok elfordulása</li>
+                                <li class="listmargin">Test helyzete</li>
+                                <li class="listmargin">Feszültség pontok meghatározása</li>
+                                <li class="listmargin">Ergonómikus mozgás mérések</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="swiper-container">
+        <div class="swiper-wrapper">
+            <div class="swiper-slide">
+                <div class="div-block-163">
+                    <div class="div-block-182">
+                        <div class="review-v1-slide-3"><img src="images/43---thumb.svg" alt="" class="image-43"></div>
+                        <div class="carcardinfohide">
+                            <div class="carcard_text">
+                                <div class="div-block-166">
+                                    <p class="paragraph-4 jarmucim">Animáció és film</p>
+                                    <p class="paragraph-43">A kézi karakter animáció egy lassú és megterhelő. Az élethű mozgás képkockánkénti megalkotása feleslegesen bonyolult folyamat. Bújj inkább a hőseid bőrébe, és add hozzá saját mozgásod a történetükhöz.</p>
+                                    <ul class="list-5">
+                                        <li class="listmargin">Animálj szabadon</li>
+                                        <li class="listmargin">Vedd fel, szerkeszd, építsd be</li>
+                                        <li class="listmargin">Akár valós időben</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-slide">
+                <div class="div-block-163">
+                    <div class="div-block-182">
+                        <div class="review-v1-slide-3"><img src="images/43---thumb.svg" alt="" class="image-43"></div>
+                        <div class="carcardinfohide">
+                            <div class="carcard_text">
+                                <div class="div-block-166">
+                                    <p class="paragraph-4 jarmucim">Játékfejlesztés</p>
+                                    <p class="paragraph-43">Semmi sem rontja el a játékélményt mint egy feltűnően nem élethűen mozgó karakter. Kerüld el a kínos helyzeteket töltsd fel a világod élettel.</p>
+                                    <ul class="list-5">
+                                        <li class="listmargin">Ültesd át az interakciókat a valóságból a virtuális világba</li>
+                                        <li class="listmargin">A ruha felhasználható kontrollerként is, irányítsd karaktereid</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="swiper-slide">
+                <div class="div-block-163">
+                    <div class="div-block-182">
+                        <div class="review-v1-slide-3"><img src="images/43---thumb.svg" alt="" class="image-43"></div>
+                        <div class="carcardinfohide">
+                            <div class="carcard_text">
+                                <div class="div-block-166">
+                                    <p class="paragraph-4 jarmucim">Orvosi</p>
+                                    <p class="paragraph-43">Ettől sokkal több szöveg nem nagyon kellene ide, max 2-3 sorral több, lehetőleg mindegyiknél egységes hosszúságú. Suspendisse varius enim in eros elementum tristique.</p>
+                                    <ul class="list-5">
+                                        <li class="listmargin">Csontok pontos fizikai helyzete</li>
+                                        <li class="listmargin">Csontok elfordulása</li>
+                                        <li class="listmargin">Test helyzete</li>
+                                        <li class="listmargin">Feszültség pontok meghatározása</li>
+                                        <li class="listmargin">Ergonómikus mozgás mérések</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="prototype-section-2 v2">
+    <div class="prototype-content az">
+        <div class="kepek-div">
+            <div data-w-id="6fa9140d-613a-472d-b562-b374ed1d7434" class="div-block-108"></div>
+            <div data-w-id="6fa9140d-613a-472d-b562-b374ed1d7435" class="div-block-109"></div>
+            <div data-w-id="6fa9140d-613a-472d-b562-b374ed1d7436" class="div-block-110"></div>
+        </div>
+        <div class="prototipus-szoveg">
+            <div class="ptext right-copy sdfsdfsd">
+                <div class="div-block-9">
+                    <h2 class="heading">Jelenleg prototípus</h2>
+                    <p class="margin-bottom">A termék jelenleg folyamatos fejlesztés alatt áll, ami lehetőséget ad, hogy a vevőink igényeire szabjuk a nekik szállított terméket. Az érdeklődőkkel szívesen együttműködünk, kipróbálást is szervezünk. A vevőinkkel hosszútávon tartjuk a kapcsolatot és reagálunk a felmerülő igényeikre. Ahogy a felhasználás módja változik úgy a termék is együtt fejlődik.<br><br>Az érdeklődök ötleteit, speciális igényeit szívesen fogadjuk, szeretnénk, hogy a felhasználóknak egy olyan eszközt tudjunk biztosítani, amivel a saját céljaikat a lehető legjobban el tudják érni</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="section-6">
+    <div class="div-block-180">
+        <div class="about-text-2">
+            <h2 class="heading">Miért velünk?</h2>
+            <p class="margin-bottom">Kreatív és kíváncsi emberek vagyunk akik szeretnek mindig az új és érdekes problémákkal foglalkozni, nem pedig a monoton ismétlődő feladatokkal tölteni az időt. Ezért fejlesztünk olyan eszközöket és megoldásokat amik a monoton munkát izgalmas és motiváló tevékenységgé alakítják.<br></p>
+        </div>
+        <div class="div-block-181"><img src="images/project_myron_prototype.jpg" alt="" class="image-42"></div>
+    </div>
+</div>
