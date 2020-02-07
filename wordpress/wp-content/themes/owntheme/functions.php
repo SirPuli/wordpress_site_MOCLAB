77cec235a9f3e0751d025ebe2c00af34b8a8e97d
Verega <60603126+Verega@users.noreply.github.com>
2020-02-07 10:50:06 +0100

    NAV menu upgrade

---
+++ wordpress/wp-content/themes/owntheme/functions.php
@@ -0,0 +1,34 @@
+ <?php 
+/**
+ * Webflow functions and definitions
+ *
+ * Set up the theme and provides some helper functions, which are used in the
+ * theme as custom template tags. Others are attached to action and filter
+ * hooks in WordPress to change core functionality.
+ *
+ * When using a child theme you can override certain functions (those wrapped
+ * in a function_exists() call) by defining them first in your child theme's
+ * functions.php file. The child theme's functions.php file is included before
+ * the parent theme's file, so the child theme functions would be used.
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ * @link https://developer.wordpress.org/themes/advanced-topics/child-themes/
+ *
+ * Functions that are not pluggable (not wrapped in function_exists()) are
+ * instead attached to a filter or action hook.
+ *
+ * For more information on hooks, actions, and filters,
+ * {@link https://developer.wordpress.org/plugins/}
+ *
+ * @package WordPress
+ * @subpackage Webflow
+ * @since Webflow
+ */
+ 
+ /*
+    REGISTER MENUS
+ */
+ register_nav_menu( 'primary', 'Primary Menu' );
+ register_nav_menu( 'home', 'Home menu' );
+ ?>
+ 
