73e601ed2524613f3f3f63d435b33fa9df1053ac
Gergő Pauli <gergopauli2@gmail.com>
2020-02-07 12:27:40 +0100

    Menu style changes

---
+++ wordpress/htdocs/wp-content/themes/moclab/tech-specs.php
@@ -0,0 +1,320 @@
+<?php get_header(); ?>
+
+<div data-collapse="medium" data-animation="default" data-duration="400" class="nav-bar-2 w-nav">
+    <div class="wrapper-4 nav-bar-wrapper">
+        <a href="index.html" class="brand-2 w-nav-brand"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/moclab.jpg" width="120" alt=""></a>
+        <div class="navigation-2">
+            <nav role="navigation" class="nav-menu w-nav-menu">
+                <a href="#" class="nav-link-4 w-nav-link"><?php _e( 'Felhasználási területek', 'moclab' ); ?></a>
+                <a href="tech-specs.html" class="nav-link-4 w-nav-link w--current"><?php _e( 'Technikai részletek', 'moclab' ); ?></a>
+                <div class="mobilkapcsolat">
+                    <a href="#" class="button-12 w-button"><?php _e( 'Get in touch', 'moclab' ); ?></a>
+                </div>
+            </nav>
+            <a href="#get-in-touch" class="button-3 small w-hidden-tiny w-button"><?php _e( 'Get in touch', 'moclab' ); ?></a>
+            <div class="menu-button-3 w-nav-button">
+                <div class="icon-4 w-icon-nav-menu"></div>
+            </div>
+        </div>
+    </div>
+    <div class="nav-bar-shadow-2"></div>
+</div>
+<div class="wrapper"></div>
+<div data-w-id="98030000-d067-cd47-7ee4-53a3ba8d5c06" class="section-copy full-screen-copy background-image-side-copy">
+    <div class="wrapper-2 techtitle">
+        <div class="row_hero-f herot">
+            <div class="col lg-6 md-3 sm-2">
+                <div class="oneliner">
+                    <h1><strong class="bold-text"><?php _e( 'Cím', 'moclab' ); ?></strong><br></h1>
+                    <p class="short-paragraph"><?php _e( 'Nem ugyanaz a szöveg mint a főoldalon, de hasonló célú. Akár 1 tömör mondat is bőven elég. Jó ha az oldal önmagában is megállja a helyét és tartalmazza a megfelelő kereső kulcsszavakat is.', 'moclab' ); ?></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="felhasznalas-1">
+    <div class="wrapper-2">
+        <h2 data-w-id="975412e2-18fa-1c9e-edec-29252b97316b" style="opacity:0" class="section-header"><?php _e( 'Felhasználási területek', 'moclab' ); ?></h2>
+        <p class="margin-bottom-3"><?php _e( 'The Brandless motion capture system ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique. Néhány felhasználási terület:', 'moclab' ); ?></p>
+        <div class="row_hero-f ft">
+            <div class="col lg-3 _2-col-in-mobile">
+                <div data-w-id="12d97a70-9c51-6b2d-a378-da6d9f2876b4" style="opacity:0" class="margin-bottom">
+                    <img src="https://uploads-ssl.webflow.com/5cdcae67e09fa50799736e99/5cdcae67e09fa5bd60736fc8_vertical%20-%20thumb.svg" width="306" alt="" class="column-image">
+                    <h4><?php _e( 'Film és animáció', 'moclab' ); ?></h4>
+                    <p class="paragraph-small"><?php _e( 'Ettől sokkal több szöveg nem nagyon kellene ide, lehetőleg mindegyiknél egységes hosszúságú. Suspendisse varius enim in eros elementum tristique.', 'moclab' ); ?></p>
+                    <a href="#" class="button-4 w-button"><?php _e( 'Tovább', 'moclab' ); ?></a>
+                </div>
+            </div>
+            <div class="col lg-3 _2-col-in-mobile">
+                <div data-w-id="12d97a70-9c51-6b2d-a378-da6d9f2876bb" style="opacity:0" class="margin-bottom">
+                    <img src="https://uploads-ssl.webflow.com/5cdcae67e09fa50799736e99/5cdcae67e09fa5bd60736fc8_vertical%20-%20thumb.svg" width="306" alt="" class="column-image">
+                    <h4><?php _e( 'Játékfejlesztés', 'moclab' ); ?></h4>
+                    <p class="paragraph-small"><?php _e( 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.', 'moclab' ); ?></p>
+                    <a href="#" class="button-4 w-button"><?php _e( 'Tovább', 'moclab' ); ?></a>
+                </div>
+            </div>
+            <div class="col lg-3 _2-col-in-mobile">
+                <div data-w-id="12d97a70-9c51-6b2d-a378-da6d9f2876c2" style="opacity:0" class="margin-bottom">
+                    <img src="https://uploads-ssl.webflow.com/5cdcae67e09fa50799736e99/5cdcae67e09fa5bd60736fc8_vertical%20-%20thumb.svg" width="306" alt="" class="column-image">
+                    <h4><?php _e( 'Orvosi', 'moclab' ); ?></h4>
+                    <p class="paragraph-small"><?php _e( 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse varius enim in eros elementum tristique.', 'moclab' ); ?></p>
+                    <a href="#" class="button-4 w-button"><?php _e( 'Tovább', 'moclab' ); ?></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="tech-specs-1">
+    <div class="specs-data">
+        <h1 class="heading-9"><?php _e( 'Technikai részletek', 'moclab' ); ?></h1>
+        <div class="data firstdata modular-info">
+            <div class="data-title">
+                <div class="data-header">
+                    <?php _e( 'Moduláris', 'moclab' ); ?>
+                </div>
+            </div>
+            <div class="data-reszlet">
+                <p class="paragraph-44"><?php _e( 'A ruha 5 egymástól független HUB-on keresztül gyűjti a mozgás adatokat és küldi el a fogadó alkalmazás felé. Ezzel a megdolással elkerüljük az egy darab HUB okozta szűk keresztmetszetet. Így egy fellépő hiba nem teszi használhatatlanná a teljes ruhát, hanem csak egy végtagot kell javítani.', 'moclab' ); ?><br><?php _e( 'Másik oldalról az egy HUB megoldás általános esetben a színész derekára kerül ami akadályozhatja a szabad mozgásban. Az elosztott rendszer a test különböző pontjaira helyei a méretben kisebb HUB-okat, kevesebb kábel fut össze egy pontba, a súlyelosztás is javul.', 'moclab' ); ?><br></p>
+            </div>
+        </div>
+        <div class="data firstdata">
+            <div class="data-title">
+                <div class="data-header">
+                    <?php _e( 'Szenzor', 'moclab' ); ?>
+                </div>
+            </div>
+            <div class="data-reszlet">
+                <ul class="list-4">
+                    <li>
+                        <?php _e( '17 db.', 'moclab' ); ?>
+                    </li>
+                    <li>
+                        <?php _e( '3x3x1cm modulok', 'moclab' ); ?>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="data">
+            <div class="data-title">
+                <div class="data-header">
+                    <?php _e( 'Hub', 'moclab' ); ?>
+                </div>
+            </div>
+            <div class="data-reszlet">
+                <ul class="list-4">
+                    <li>
+                        <?php _e( '6x5x2cm hubok a karokon, lábakon és a derékon', 'moclab' ); ?>
+                        <br>
+                    </li>
+                    <li>
+                        <?php _e( '5 db.', 'moclab' ); ?>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="data">
+            <div class="data-title">
+                <div class="data-header">
+                    <?php _e( 'Battery', 'moclab' ); ?>
+                </div>
+            </div>
+            <div class="data-reszlet">
+                <ul class="list-4">
+                    <li>
+                        <?php _e( '750mAh, 5-6 óra folymamatos használat.', 'moclab' ); ?>
+                    </li>
+                    <li>
+                        <?php _e( 'Minden hubon Mini USB töltő', 'moclab' ); ?>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="data">
+            <div class="data-title">
+                <div class="data-header">
+                    <?php _e( 'Kábelek', 'moclab' ); ?>
+                </div>
+            </div>
+            <div class="data-reszlet">
+                <ul class="list-4">
+                    <li>
+                        <?php _e( 'flexibilis kábelek, micro USB csatlakozók', 'moclab' ); ?>
+                        <br>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="data">
+            <div class="data-title">
+                <div class="data-header">
+                    <?php _e( 'Ruha, rögzítés', 'moclab' ); ?>
+                </div>
+            </div>
+            <div class="data-reszlet">
+                <ul class="list-4">
+                    <li>
+                        <?php _e( 'Egymástól független gumis pántok. Nincs fix ruha méret, szabadon változtatható, testalkatok széles skálájával használható', 'moclab' ); ?>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="data">
+            <div class="data-title">
+                <div class="data-header">
+                    <?php _e( 'Connectivity', 'moclab' ); ?>
+                </div>
+            </div>
+            <div class="data-reszlet">
+                <ul class="list-4">
+                    <li>
+                        <strong><?php _e( 'Wifi', 'moclab' ); ?></strong>
+                        <br>
+                    </li>
+                    <li>
+                        <?php _e( 'Laptop vagy akár mobil telefonnal is', 'moclab' ); ?>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="data">
+            <div class="data-title">
+                <div class="data-header">
+                    <?php _e( 'Plugins', 'moclab' ); ?>
+                </div>
+            </div>
+            <div class="data-reszlet">
+                <ul class="list-4">
+                    <li>
+                        <?php _e( '3ds max', 'moclab' ); ?>
+                    </li>
+                    <li>
+                        <?php _e( 'Unreal', 'moclab' ); ?>
+                    </li>
+                    <li>
+                        <?php _e( 'Unity', 'moclab' ); ?>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="data">
+            <div class="data-title">
+                <div class="data-header">
+                    <?php _e( 'Format', 'moclab' ); ?>
+                </div>
+            </div>
+            <div class="data-reszlet">
+                <ul class="list-4">
+                    <li>
+                        <?php _e( '3ds max interface-en keresztül minden ott elérhető fájlformátum', 'moclab' ); ?>
+                        <br>
+                    </li>
+                    <li>
+                        <?php _e( 'BVH', 'moclab' ); ?>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<div id="get-in-touch" class="section-7">
+    <div class="div-block-185">
+        <h1 class="heading-10"><?php _e( 'Let&#x27;s connect!', 'moclab' ); ?></h1>
+        <div data-duration-in="400" data-duration-out="100" data-easing="ease-in-out" class="tabs-2 w-tabs">
+            <div class="tabs-menu w-tab-menu">
+                <a data-w-tab="Tab 1" class="tab-link-tab-1 w-inline-block w-tab-link w--current"> <div class="text-block-43">
+                        <?php _e( 'Join the Mailinglist', 'moclab' ); ?>
+                    </div> </a>
+                <a data-w-tab="Tab 2" class="tab-link-tab-2 w-inline-block w-tab-link"> <div class="text-block-42">
+                        <?php _e( 'Write us a message', 'moclab' ); ?>
+                    </div> </a>
+            </div>
+            <div class="w-tab-content">
+                <div data-w-tab="Tab 1" class="w-tab-pane w--tab-active">
+                    <div class="div-block-186">
+                        <p class="paragraph-small ctap-tabs"><?php _e( 'Ha felkeltettük az érdeklődésed és szeretnél velünk kapcsolatba lépni, többet megtudni a termékről, a fejlesztés folyamatáról, szeretnéd élőben látni, kipróbálni a ruhát, iratkozz fel hírlevelünkre!', 'moclab' ); ?></p>
+                        <div class="col lg-4 sm-2">
+                            <div class="contact-form-container light-blue padding">
+                                <div class="form w-form">
+                                    <form id="email-form" name="email-form" data-name="Email Form" class="form-2">
+                                        <input type="text" class="form-field w-input" maxlength="256" name="email-2" data-name="Email 2" placeholder="Enter your email" id="email-2" required="">
+                                        <input type="submit" value="Sign up" data-wait="Please wait..." class="button w-button">
+                                    </form>
+                                    <div class="w-form-done">
+                                        <div>
+                                            <?php _e( 'Thank you! Your submission has been received!', 'moclab' ); ?>
+                                        </div>
+                                    </div>
+                                    <div class="w-form-fail">
+                                        <div>
+                                            <?php _e( 'Oops! Something went wrong while submitting the form.', 'moclab' ); ?>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div data-w-tab="Tab 2" class="w-tab-pane">
+                    <div class="div-block-187">
+                        <div class="contactform-accordion">
+                            <div wp="general" el="form" class="form-wrap-2 w-form">
+                                <form id="email-form" name="email-form" data-name="Email Form" class="form-4">
+                                    <div class="field-wrap-2 name">
+                                        <label for="Nev-3" class="form-field-label">
+                                            <?php _e( 'Név', 'moclab' ); ?>
+                                        </label>
+                                        <input type="text" class="form-field-3 w-input" autocomplete="off" maxlength="256" name="Nev-2" data-name="Nev 2" placeholder="Vezetéknév Keresztnév" id="Nev-2" required="">
+                                    </div>
+                                    <div class="field-wrap-2 email">
+                                        <label for="Email-4" class="form-field-label">
+                                            <?php _e( 'Email cím', 'moclab' ); ?>
+                                        </label>
+                                        <input type="email" class="form-field-3 w-input" autocomplete="off" maxlength="256" name="Email-3" data-name="Email 3" placeholder="teszt@elek.com" id="Email-3" required="">
+                                    </div>
+                                    <div class="field-wrap-2 message">
+                                        <label for="Uzenet-3" class="form-field-label">
+                                            <?php _e( 'Üzenet', 'moclab' ); ?>
+                                        </label>
+                                        <textarea name="Uzenet-2" maxlength="5000" id="Uzenet-2" data-name="Uzenet 2" class="textarea-2 w-input"></textarea>
+                                    </div>
+                                    <input type="submit" value="Küldés" data-wait="Kérlek várj..." class="submit-button-2 w-button">
+                                </form>
+                                <div class="w-form-done">
+                                    <div>
+                                        <?php _e( 'Köszönjük! Munkatársunk hamarosan felveszi veled a kapcsolatot!', 'moclab' ); ?>
+                                    </div>
+                                </div>
+                                <div class="w-form-fail">
+                                    <div>
+                                        <?php _e( 'Oops! Something went wrong while submitting the form.', 'moclab' ); ?>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<footer class="section-3">
+    <div class="wrapper-3">
+        <div class="flex-horizontal-space-between footer">
+            <div class="div-block-183">
+                <div>
+                    <?php _e( '2019 copyright blabla', 'moclab' ); ?>
+                </div>
+            </div>
+            <div class="social-media-icons-container-2">
+                <a href="#" class="footer-link no-padding first w-inline-block"><img src="https://uploads-ssl.webflow.com/5cdcae67e09fa50799736e99/5cdcae67e09fa56959736f94_twitter.svg" alt="" class="social-media-icon"></a>
+                <a href="#" class="footer-link no-padding w-inline-block"></a>
+                <a href="#" class="footer-link no-padding w-inline-block"><img src="https://uploads-ssl.webflow.com/5cdcae67e09fa50799736e99/5cdcae67e09fa5629b736f9f_instagram.svg" alt="" class="social-media-icon"></a>
+                <a href="#" class="footer-link no-padding w-inline-block"></a>
+                <a href="#" class="footer-link no-padding w-inline-block"><img src="https://uploads-ssl.webflow.com/5cdcae67e09fa50799736e99/5cdcae67e09fa571fb736fb7_facebook.svg" alt="" class="social-media-icon"></a>
+                <a href="#" class="footer-link no-padding last w-inline-block"></a>
+            </div>
+        </div>
+    </div>
+</footer>        
+
+<?php get_footer(); ?>
